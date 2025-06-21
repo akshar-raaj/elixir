29ad1a22289f0c01b74be78890bd4e4922cdbae5
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-21 23:27:07 +0530

    Arithmetic operations and other functions like round, rem, div, trunc etc.

---
+++ arithmetic.exs
@@ -0,0 +1,22 @@
+IO.puts("Performing some arithmetic operations! We will perform\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Rounding\n6.Remainder\n7.Truncation")
+
+first = 10
+second = 20
+
+sum = first + second
+IO.puts("Sum is #{sum}")
+
+difference = second - first
+IO.puts("Difference is #{difference}")
+
+product = first * second
+IO.puts("Product is #{product}")
+
+rounded = round(5.2)
+IO.puts("Rounded: #{rounded}")
+
+remainder = rem 10, 6
+IO.puts("Remainder: #{remainder}")
+
+truncated = trunc(3.5)
+IO.puts("Integer part of number: #{truncated}")
