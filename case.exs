71ef1820602af93734b1b70666af432ae25bbce9
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-23 15:45:12 +0530

    Comparison operators with case.

--- case.exs
+++ case.exs
@@ -1,7 +1,22 @@
-is_success = :error
+# Basic usage
+
+is_success = :ok
 case is_success do
   result when result == :ok -> 
     IO.puts("Success")
   result when result == :error ->
     IO.puts("Error")
 end
+
+# Case with comparison operators
+# when adds a 'guard'
+age = 70
+case age do
+  x when x >= 65 ->
+    IO.puts("Senior citizen")
+  x when x >= 18 ->
+    IO.puts("Adult")
+  x when x < 18 ->
+    IO.puts("Minor")
+end
+
