80c1091e6740c73881624614271fce1ce64c330c
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-23 13:57:35 +0530

    Modules and Functions.

---
+++ ascend.exs
@@ -0,0 +1,12 @@
+# Module and Functions exploration
+defmodule Ascend do
+  def about() do
+    "Stay Independent, Ascend together!"
+  end
+
+  def leadership() do
+    ["David Wurtzbacher", "Prasad Sristi"]
+  end
+end
+
+IO.puts Ascend.leadership()
