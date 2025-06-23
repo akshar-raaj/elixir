90a9e94b34480d8843e2cb0fc5c01e3b784dbc30
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-23 13:10:51 +0530

    Update an existing key using pipe, and assertive programming style using . operator.

--- map.exs
+++ map.exs
@@ -8,3 +8,10 @@ IO.puts("Headquarters: #{Map.get(information, :headquarters)}")
 # Update the map with a new key.
 information = Map.put(information, :founder, "David Wurtzbacher")
 IO.puts("Founder: #{information[:founder]}")
+
+# Update an existing key, pipe symbol used here.
+information = %{information | :name => "Ascend"}
+
+# Another way of accessing the key using . operator
+name = information.name
+IO.puts("Name: #{name}")
