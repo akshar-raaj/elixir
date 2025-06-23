d1a966db580b84207fe597c180f0f4d28af7b586
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-23 12:47:36 +0530

    Multi-line anonymous functions are possible, unlike Python's lambda.

--- anonymous_fun.exs
+++ anonymous_fun.exs
@@ -2,6 +2,12 @@ concat = fn value -> "Edited " <> value <> " !" end
 
 len = fn list -> length list end
 
+# Unlike Python, multi line anonymous functions are possible.
+multi_line_triple = fn x ->
+    double = x + x
+    double + x
+  end
+
 
 firm_one = "Opsahl Dawson"
 firm_two = "Walter Shuffain"
@@ -11,3 +17,5 @@ IO.puts(concat.(firm_one))
 IO.puts(concat.(firm_two))
 
 IO.puts("Length: #{len.(firms)}")
+
+IO.puts("Triple 3: #{multi_line_triple.(3)}")
