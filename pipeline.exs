a3a1afba9606f9a9035719c7a37d8850b21ab3cd
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-23 18:32:47 +0530

    Slightly advanced example using \& for function and argument capture.

--- pipeline.exs
+++ pipeline.exs
@@ -3,3 +3,10 @@ range = 1..10
 # Define a pipeline
 doubles = range |> Enum.map(fn x -> x * 2 end)
 IO.inspect doubles
+
+
+# Slightly advanced.
+# Uses & twice, once for function capture and inside it to refer to argument.
+
+triples = range |> Enum.map(&(&1 * 3))
+IO.inspect triples
