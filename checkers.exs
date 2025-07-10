648044044ef2693c1c6b4c421f151ad6f533a9e8
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-10 14:31:41 +0530

    Added checkers is_tuple and is_map.

--- checkers.exs
+++ checkers.exs
@@ -4,6 +4,8 @@ string = "opsahl dawson"
 boolean = true
 list = ["odc", "atkg", "levit zacks"]
 keyword_list = [name: "sentient", address: "Hyderabad"]
+tuple = {1, 2, 3}
+map = %{name: "John", age: 30}
 
 IO.puts("#{integer} is an integer? #{is_integer(integer)}")
 IO.puts("#{floating} is a float? #{is_float(floating)}")
@@ -11,3 +13,5 @@ IO.puts("#{string} is a string? #{is_binary(string)}")
 IO.puts("#{list} is a list? #{is_list(list)}")
 result = is_list(keyword_list)
 IO.puts("Keyword list is a list? #{result}")
+IO.puts("#{inspect(tuple)} is a tuple? #{is_tuple(tuple)}")
+IO.puts("#{inspect(map)} is a map? #{is_map(map)}")
