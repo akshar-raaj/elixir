6104a67b4267904a481b6b13f145fae97ce0f551
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-10 14:27:09 +0530

    Case demonstration without guards. Will add a separate script to demonstrate guards.

--- case.exs
+++ case.exs
@@ -1,22 +1,12 @@
-# Basic usage
+# Basic case usage with exact pattern matching
 
-is_success = :ok
-case is_success do
-  result when result == :ok -> 
-    IO.puts("Success")
-  result when result == :error ->
-    IO.puts("Error")
-end
-
-# Case with comparison operators
-# when adds a 'guard'
-age = 70
-case age do
-  x when x >= 65 ->
-    IO.puts("Senior citizen")
-  x when x >= 18 ->
-    IO.puts("Adult")
-  x when x < 18 ->
-    IO.puts("Minor")
+name = "struva"
+case name do
+  "struva" -> 
+    IO.puts("My name is Struva")
+  "apaar" -> 
+    IO.puts("I am Apaar")
+  _ -> 
+    IO.puts("Unknown name")
 end
 
